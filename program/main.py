3ba4c8a32a5d7ee20475885263b3abf44bc1263b
Totalpolarfox <totalpolarfox@outlook.com>
2024-08-13 19:55:56 +0300

    Added main program

---
+++ program/main.py
@@ -0,0 +1,11 @@
+from menu import Menu
+
+# красота в виде цветного текста не использована
+# предполагается, что код будет читаться без запуска
+
+def main():
+    menu = Menu()
+    menu.display()
+
+if __name__ == "__main__":
+    main()
